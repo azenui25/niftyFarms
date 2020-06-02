9f994f31ddead4341f53e6ee63887ee77afbd010
azenui25 <asanga.nicholas@yahoo.com>
2020-06-02 13:54:48 +0200

    created user login with mongodb

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,14 @@ import './App.css';
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen';
+import SigninScreen from './screens/SigninScreen';
+import { useSelector } from 'react-redux'
 
 function App() {
+
+  const userSignin = useSelector(state => state.userSignin);
+  const { userInfo } = userSignin;
+
   const openMenu = () => {
     document.querySelector(".sidebar").classList.add("open");
   }
@@ -24,7 +30,11 @@ function App() {
       </div>
       <div className="header-links">
         <a href="cart.html">Cart</a>
-        <a href="signin.html">Sign In</a>
+        {
+              userInfo ? <Link to="/profile">{userInfo.name}</Link> :
+                <Link to="/signin">Sign In</Link>
+            }
+            
       </div>
     </header>
     <aside className="sidebar">
@@ -43,6 +53,7 @@ function App() {
     </aside>
     <main className="main">
       <div className="content">
+      <Route path="/signin" component={SigninScreen} />
       <Route path='/product/:id' component={ProductScreen}/>
       <Route path="/cart/:id?" component={CartScreen} />
       <Route path='/' exact={true} component={HomeScreen}/>
